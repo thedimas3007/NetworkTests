e36bb32f3eb398a9bf9f882c36c4a9c4ac00c4bc
thedimas3007 <dima.orischenko@gmail.com>
2023-08-17 21:55:48 +0200

    basic TestClient implementation

--- src/main/java/thedimas/network/test/TestClient.java
+++ src/main/java/thedimas/network/test/TestClient.java
@@ -1,7 +1,15 @@
 package thedimas.network.test;
 
+import thedimas.network.client.Client;
+import thedimas.network.packet.PlayerPacket;
+
+import java.io.IOException;
+
 public class TestClient {
-    public static void main(String[] args) {
-        throw new UnsupportedOperationException("Not implemented"); // TODO: implement
+    public static void main(String[] args) throws IOException {
+        Client client = new Client("127.0.0.1", 9999);
+        client.connect();
+        client.send(new PlayerPacket());
+        client.disconnect();
     }
 }
